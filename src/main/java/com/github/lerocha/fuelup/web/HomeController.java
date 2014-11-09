8b73fe2180cb87379bab9751159ccfed18f39c70
Luis Rocha <lerocha@gmail.com>
2014-11-08 22:22:16 -0800

    adding logging

--- src/main/java/com/github/lerocha/fuelup/web/HomeController.java
+++ src/main/java/com/github/lerocha/fuelup/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.github.lerocha.fuelup.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
+    private static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping("/")
     public String home(@RequestParam(value="name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
+        logger.info("home page request; name={}", name);
         return "home";
     }
 }
