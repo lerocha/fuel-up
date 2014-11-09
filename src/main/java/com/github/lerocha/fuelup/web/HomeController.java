bb773b34747cf5663989299b7119f36710b8e253
Luis Rocha <lerocha@gmail.com>
2014-11-08 18:48:38 -0800

    Serving Web content with Spring & Thymeleaf (http://spring.io/guides/gs/serving-web-content/)

--- src/main/java/com/github/lerocha/fuelup/web/HomeController.java
+++ src/main/java/com/github/lerocha/fuelup/web/HomeController.java
@@ -1,13 +1,16 @@
 package com.github.lerocha.fuelup.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    public String home(@RequestParam(value="name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
     }
 }
